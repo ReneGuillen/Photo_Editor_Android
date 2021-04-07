0e529a37db650b32ee3e060fff56178b40738a75
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 13:14:26 -0500

    Adding Ratio Options

--- src/main/java/com/ProfileFit/RatioFragment.java
+++ src/main/java/com/ProfileFit/RatioFragment.java
@@ -5,36 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.software.ProfileFit.Adapter.RatioAdapter;
 
+//Class to add ratio options functionality to the app.
 public class RatioFragment extends Fragment implements RatioAdapter.RatioAdapterListener {
 
-    RecyclerView recycler_ratio;
+    private static RecyclerView recycler_ratio;
+    private static RatioAdapter.RatioAdapterListener listener;
+    private static int background_selected = -1;
+    private static FiltersListFragment instance;
+    private static Bitmap bitmap;
 
-    RatioAdapter.RatioAdapterListener listener;
-
-
-    int background_selected = -1;
-
-    static FiltersListFragment instance;
-    //static RatioFragment instance;
-    static Bitmap bitmap;
-
+    //Get instace from the bitmap/image selected to work with.
     public static FiltersListFragment getInstance(Bitmap bitmapSave) {
         bitmap = bitmapSave;
-
         if(instance == null)
-        {
             instance = new FiltersListFragment();
-        }
-
         return instance;
     }
-
+    
+    //Set listener to the ratio adapter.
     public void setListener(RatioAdapter.RatioAdapterListener listener) {
         this.listener = listener;
     }
@@ -42,32 +35,27 @@ public class RatioFragment extends Fragment implements RatioAdapter.RatioAdapter
     public RatioFragment() {
         // Required empty public constructor
     }
-
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
+    //Method to handle to view and add the ratio fragment layout, recycle unused items.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View itemView = inflater.inflate(R.layout.fragment_ratio, container, false);
-
         recycler_ratio = (RecyclerView) itemView.findViewById(R.id.recycler_ratio);
-
-
         recycler_ratio.setHasFixedSize(true);
         recycler_ratio.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         recycler_ratio.setAdapter(new RatioAdapter(getContext(),this));
-
-
+        //Return new view.
         return  itemView;
     }
 
+    //Handle the requests when a ratio option is seleted.
     @Override
     public void onRatioSelected(int ratio) {
         if(listener != null)
