dc41076c76009a32724276c6ef97743799a736d3
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 13:04:09 -0500

    Updating Filters Fragment

--- src/main/java/com/ProfileFit/FiltersListFragment.java
+++ src/main/java/com/ProfileFit/FiltersListFragment.java
@@ -5,17 +5,14 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.software.ProfileFit.Adapter.ThumbnailAdapter;
 import com.software.ProfileFit.Interface.FirltersListFragmentListener;
 import com.software.ProfileFit.Utils.BitmapUtils;
@@ -24,25 +21,21 @@ import com.zomato.photofilters.FilterPack;
 import com.zomato.photofilters.imageprocessors.Filter;
 import com.zomato.photofilters.utils.ThumbnailItem;
 import com.zomato.photofilters.utils.ThumbnailsManager;
-
 import java.util.ArrayList;
 import java.util.List;
 
- //A simple {@link Fragment} subclass.
-
+//Filter Class Fragment.
 public class FiltersListFragment extends Fragment implements FirltersListFragmentListener {
-    RecyclerView recyclerView;
-    ThumbnailAdapter adapter;
-    List<ThumbnailItem> thumbnailItems;
+    private static RecyclerView recyclerView;
+    private static ThumbnailAdapter adapter;
+    private static List<ThumbnailItem> thumbnailItems;
+    private static FirltersListFragmentListener listener;
+    private static FiltersListFragment instance;
+    private static Bitmap bitmap;
 
-    FirltersListFragmentListener listener;
-
-    static FiltersListFragment instance;
-    static Bitmap bitmap;
-
+    //Get initial instace from the saved bitmap.
     public static FiltersListFragment getInstance(Bitmap bitmapSave) {
         bitmap = bitmapSave;
-
         if(instance == null)
         {
             instance = new FiltersListFragment();
@@ -51,6 +44,7 @@ public class FiltersListFragment extends Fragment implements FirltersListFragmen
         return instance;
     }
 
+    //Main listener set to the filters list fragment.
     public void setListener(FirltersListFragmentListener listener) {
         this.listener = listener;
     }
@@ -59,47 +53,38 @@ public class FiltersListFragment extends Fragment implements FirltersListFragmen
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    //Create the new view from filters list, recycle unused elements and display thumnail.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View itemView = inflater.inflate(R.layout.fragment_filters_list, container, false);
-
         thumbnailItems = new ArrayList<>();
         adapter = new ThumbnailAdapter(thumbnailItems, this, getActivity());
-
         recyclerView = (RecyclerView) itemView.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
-        //recyclerView.setItemAnimator(new DefaultItemAnimator());
-        //int space = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8,getResources().getDisplayMetrics());
-        //recyclerView.addItemDecoration(new SpacesItemDecoration(space));
         recyclerView.setAdapter(adapter);
-
         displayThumbnail(bitmap);
-
+        //Return the new item view.
         return  itemView;
     }
 
+   //Method to display a thumbnail based on the parameter bitmap passed.
     public void displayThumbnail(final Bitmap bitmap) {
         Runnable r = new Runnable() {
             @Override
             public void run() {
                 Bitmap thumbImg;
                 if(bitmap == null){
-                    //thumbImg = BitmapFactory.decodeResource(getResources(), R.drawable.filter_image);
                     thumbImg = BitmapUtils.decodeSampledBitmapFromResource(getResources(),R.drawable.filter_image, 50,50);
                 }
                 else {
-                    //thumbImg = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
                     thumbImg = BitmapUtils.decodeSampledBitmapFromResource(getResources(),R.drawable.filter_image, 50,50);
-                    //thumbImg = BitmapFactory.decodeResource(getResources(), R.drawable.filter_image);
-                    //thumbImg = BitmapUtils.getBitmapFromAssets(getActivity(),Editor.pictureName, 100, 100 );
                 }
                 if(thumbImg == null)
                     return;
@@ -112,7 +97,6 @@ public class FiltersListFragment extends Fragment implements FirltersListFragmen
                 thumbnailItem.filterName = "normal";
                 ThumbnailsManager.addThumb(thumbnailItem);
 
-                //List<Filter> filters = FilterPack.getFilterPack(getActivity());
                 List<Filter> filters = PackofFilters.getFilterPack(getActivity());
 
                 for(Filter filter:filters)
@@ -136,6 +120,7 @@ public class FiltersListFragment extends Fragment implements FirltersListFragmen
         new Thread(r).start();
     }
 
+    //Handle filter selection when a new filter is selected.
     @Override
     public void onFrilterSelected(Filter filter) {
         if(listener != null)
