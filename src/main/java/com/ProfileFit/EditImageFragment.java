8e1926414b5912041656fc77693cab2aaeded889
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 12:16:24 -0500

    Adding Background Functionality.

--- src/main/java/com/ProfileFit/EditImageFragment.java
+++ src/main/java/com/ProfileFit/EditImageFragment.java
@@ -2,47 +2,36 @@ package com.software.ProfileFit;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.software.ProfileFit.Adapter.BackgroundAdapter;
 import com.software.ProfileFit.Interface.EditImageFragmentListener;
 
-
+//Background class to take care of all funtions onTouched.
 public class EditImageFragment extends Fragment implements BackgroundAdapter.BackgroundAdapterListener {
-
-
-
-    RecyclerView recycler_background;
-    Button button;
-
-    EditImageFragmentListener listener;
-
-
-    int background_selected = -1;
-
-    static FiltersListFragment instance;
-    static Bitmap bitmap;
-
+    private static RecyclerView recycler_background;
+    private static Button button;
+    private static EditImageFragmentListener listener;
+    private static int background_selected = -1;
+    private static FiltersListFragment instance;
+    private static Bitmap bitmap;
+    
+    //Set initial bitmap list based on fragment.
     public static FiltersListFragment getInstance(Bitmap bitmapSave) {
         bitmap = bitmapSave;
-
         if(instance == null)
-        {
             instance = new FiltersListFragment();
-        }
 
         return instance;
     }
 
+    //Initialize listener to fragment listerner.
     public void setListener(EditImageFragmentListener listener) {
         this.listener = listener;
     }
@@ -51,31 +40,27 @@ public class EditImageFragment extends Fragment implements BackgroundAdapter.Bac
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
+    //Create the new view to work with the background features.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View itemView = inflater.inflate(R.layout.fragment_edit_image, container, false);
-
+        //Recycle elements that wont be used.
         recycler_background = (RecyclerView) itemView.findViewById(R.id.recycler_background);
-
-
         recycler_background.setHasFixedSize(true);
         recycler_background.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         recycler_background.setAdapter(new BackgroundAdapter(getContext(),this));
-
-
+        //Return new itemView after recycling.
         return  itemView;
     }
 
+    //Method to handle background selections.
     @Override
     public void onBackgroundSelected(int background) {
         if(listener != null)
@@ -84,6 +69,7 @@ public class EditImageFragment extends Fragment implements BackgroundAdapter.Bac
             listener.onAddBackground(background);
     }
 
+    //Method to change to the next fragment when slide to the next window.
     private void enterNextFragment() {
         BorderFragment borderFragment = new BorderFragment();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
